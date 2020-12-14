a983e09eb2af1f2e5be47e039b96ea7435dab93f
cristobalzumaran <75954687+cristobalzumaran@users.noreply.github.com>
2020-12-13 23:10:27 -0600

    segundo commit

--- src/test/java/comDevOps/Main.java
+++ src/test/java/comDevOps/Main.java
@@ -1,21 +1,16 @@
 package comDevOps;
 
-import java.util.Scanner;
-
 public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Comenzado Prueba JENKINS");
 
-		Scanner reader = new Scanner(System.in);
-		int numero1 = 0;
-		int numero2 = 0;
+		int numero1 = 5;
+		int numero2 = 8;
 		  
-		System.out.println("Introduce el primer número:");      
-		numero1 = reader.nextInt();
+		System.out.println("Introduce el primer número:"+numero1);      
 		      
-		System.out.println("Introduce el segundo número:");
-		numero2 = reader.nextInt();
+		System.out.println("Introduce el segundo número:"+numero2);
 
 		 int resultado = numero1+numero2;
 
